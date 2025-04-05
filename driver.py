09c2c0f0abebba635c4bd2357ef041aa71b0b4f9
Isaac <isaac.rice66@gmail.com>
2025-04-05 19:25:12 +1030

    Driver added, data collected

--- driver.py
+++ driver.py
@@ -1,8 +1,45 @@
 #very incomplete
 
-import trader
+from trader import Trader
+from typing import List, Dict
 from datamodel import TradingState, Listing
+import pandas as pd
+
+def test(timestamp: int, past_state: TradingState):
+    state: TradingState = TradingState(past_state.traderData)
+
+def main():
+    test: str = "kelp-1.csv"
+    test_data = pd.read_csv(test)
+    trader: Trader
+
+class back_tester:
+    test_data: pd.core.frame.DataFrame
+    time_stamps: List[int]
+    states: Dict[int, TradingState]
+    trader: Trader
+
+    def __init__(self, data_file : str):
+        self.test_data = pd.read_csv('data/'+data_file,sep=';')
+        self.time_stamps = list(self.test_data['timestamps'].drop_duplicates())
+        for time in self.time_stamps:
+            relevant_frame = self.test_data[self.test_data['timestamp']==time]
+            state: TradingState = TradingState()
+            state.timestamp = time
+            for row in relevant_frame:
+                #map stamp to tradingstate
+                state.
+        self.time_stamp = 0
+        self.test()
+
+    def test(self, state: TradingState):
+        traderData = state.traderData
+        old_time = state.timestamp
+
+        result, conversions, traderData = self.trader.run(state)
+
+
+    
 
 if __name__=="__main__":
-    ts : TradingState = TradingState("",0,{"RAINFOREST_RESIN": Listing("RAINFOREST_RESIN",)})
-    trader.run()
+    initialTrade : TradingState = TradingState("",0,)
--- trader.py
+++ trader.py
@@ -16,15 +16,17 @@ class Trader:
         best_bid, best_bid_amount = list(state.order_depths["RAINFOREST_RESIN"].buy_orders.items())[0]
         orders: List[Order] = []
         # just around right these values def need adjusting -- find through AS
-        MAX_ASK = 10000
-        MIN_BID = 10000
-        if int(best_ask) < MAX_ASK:
-            orders.append(Order("RAINFOREST_RESIN", best_ask, -best_ask_amount))
-            print("BUYING " + str(-best_ask_amount) + " SHARES AT ", best_ask)
-        if int(best_bid) > MIN_BID:
-            orders.append(Order("RAINFOREST_RESIN", best_bid, -best_bid_amount))
-            print("SELLING " + str(-best_bid_amount) + " SHARES AT ", best_bid)
-        
+        # MAX_ASK = 9999
+        # MIN_BID = 10001
+        # if int(best_ask) < MAX_ASK:
+        #     orders.append(Order("RAINFOREST_RESIN", best_ask, -best_ask_amount))
+        #     print("BUYING " + str(-best_ask_amount) + " SHARES AT ", best_ask)
+        # if int(best_bid) > MIN_BID:
+        #     orders.append(Order("RAINFOREST_RESIN", best_bid, -best_bid_amount))
+        #     print("SELLING " + str(-best_bid_amount) + " SHARES AT ", best_bid)
+        orders.append(Order("RAINFOREST_RESIN", 9998, 25))
+        orders.append(Order("RAINFOREST_RESIN", 10002, -25))
+        result["RAINFOREST_RESIN"] = orders
         traderData = state.traderData
         '''
         result["RAINFOREST_RESIN"] = orders
@@ -32,6 +34,6 @@ class Trader:
         if traderData.count(';') < 4:
             traderData += ';' + str()'
         '''
-        
+        print(str(state.market_trades))
         conversions = None
         return result, conversions, traderData
