15c9541e21c93f3facf44cfdd795444092b95b91
Isaac <isaac.rice66@gmail.com>
2025-04-11 00:02:55 +0930

    created template which allows strategies for different stocks

--- TEMPLATE_DOC.py
+++ TEMPLATE_DOC.py
@@ -2,114 +2,11 @@ import json
 from abc import abstractmethod
 from collections import deque
 from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
-from typing import Any
+from typing import *
+import pandas as pd
 
-# JSON: TypeAlias = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
+MACDsignals = List[int]
 
-# class Logger:
-#     def __init__(self) -> None:
-#         self.logs = ""
-#         self.max_log_length = 3750
-
-#     def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
-#         self.logs += sep.join(map(str, objects)) + end
-
-#     def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
-#         base_length = len(self.to_json([
-#             self.compress_state(state, ""),
-#             self.compress_orders(orders),
-#             conversions,
-#             "",
-#             "",
-#         ]))
-
-#         # We truncate state.traderData, trader_data, and self.logs to the same max. length to fit the log limit
-#         max_item_length = (self.max_log_length - base_length) // 3
-
-#         print(self.to_json([
-#             self.compress_state(state, self.truncate(state.traderData, max_item_length)),
-#             self.compress_orders(orders),
-#             conversions,
-#             self.truncate(trader_data, max_item_length),
-#             self.truncate(self.logs, max_item_length),
-#         ]))
-
-#         self.logs = ""
-
-#     def compress_state(self, state: TradingState, trader_data: str) -> list[Any]:
-#         return [
-#             state.timestamp,
-#             trader_data,
-#             self.compress_listings(state.listings),
-#             self.compress_order_depths(state.order_depths),
-#             self.compress_trades(state.own_trades),
-#             self.compress_trades(state.market_trades),
-#             state.position,
-#             self.compress_observations(state.observations),
-#         ]
-
-#     def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
-#         compressed = []
-#         for listing in listings.values():
-#             compressed.append([listing["symbol"], listing["product"], listing["denomination"]])
-
-#         return compressed
-
-#     def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
-#         compressed = {}
-#         for symbol, order_depth in order_depths.items():
-#             compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
-
-#         return compressed
-
-#     def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
-#         compressed = []
-#         for arr in trades.values():
-#             for trade in arr:
-#                 compressed.append([
-#                     trade.symbol,
-#                     trade.price,
-#                     trade.quantity,
-#                     trade.buyer,
-#                     trade.seller,
-#                     trade.timestamp,
-#                 ])
-
-#         return compressed
-
-#     def compress_observations(self, observations: Observation) -> list[Any]:
-#         conversion_observations = {}
-#         for product, observation in observations.conversionObservations.items():
-#             conversion_observations[product] = [
-#                 observation.bidPrice,
-#                 observation.askPrice,
-#                 observation.transportFees,
-#                 observation.exportTariff,
-#                 observation.importTariff,
-#             ]
-
-#         return [observations.plainValueObservations, conversion_observations]
-
-#     def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
-#         compressed = []
-#         for arr in orders.values():
-#             for order in arr:
-#                 compressed.append([order.symbol, order.price, order.quantity])
-
-#         return compressed
-
-#     def to_json(self, value: Any) -> str:
-#         return json.dumps(value, cls=ProsperityEncoder, separators=(",", ":"))
-
-#     def truncate(self, value: str, max_length: int) -> str:
-#         if len(value) <= max_length:
-#             return value
-
-#         return value[:max_length - 3] + "..."
-
-# logger = Logger()
-
-# Base strategy class for all trading strategies
 class Strategy:
     def __init__(self, symbol: str, limit: int) -> None:
         self.symbol = symbol  # The trading symbol
@@ -235,8 +132,114 @@ class MarketMakingStrategy(Strategy):
         # Restore the liquidation window from saved state
         self.window = deque(data)
 
+class MACD(Strategy):
+    def __init__(self, symbol, limit, max_sample_size):
+        super().__init__(symbol, limit)
+        self.past_data: List[float] = []
+        self.max_sample_size = max_sample_size
 
-# Strategy for AMETHYSTS: assumes a fixed fair value
+    def load(self, data: List[float]) -> None:
+        self.past_data = data
+
+    def save(self):
+        return self.past_data
+
+    # AUXILLARY FUNCTIONS
+    def MACD(self, state: TradingState, ema_fast: int=12, 
+        ema_slow: int=26, ema_sig: int=9) -> MACDsignals:
+        # signal-line crossover, zero crossover and divergence respectively
+        signals : MACDsignals = [0, 0, 0]
+        if self.past_data is None or len(self.past_data) < (ema_fast + ema_slow + 1):
+            return signals
+        ema_fast = pd.Series(self.past_data).ewm(span=ema_fast, adjust=False).mean()
+        ema_slow = pd.Series(self.past_data).ewm(span=ema_slow, adjust=False).mean()
+        macd = ema_fast - ema_slow
+        signal = macd.ewm(span=ema_sig, adjust=False).mean()
+        hist = macd - signal
+        # will implement lookback later
+        # signal-line crossover
+        if hist.iloc[-1] > 0 and hist.iloc[-2] < 0:
+            signals[0] = 1
+        elif hist.iloc[-1] < 0 and hist.iloc[-2] > 0:
+            signals[0] = -1
+        else:
+            signals[0] = 0
+        # zero crossover
+        if macd.iloc[-1] > 0 and macd.iloc[-2] < 0:
+            signals[1] = 1
+        elif macd.iloc[-1] < 0 and macd.iloc[-2] > 0:
+            signals[1] = -1
+        else:
+            signals[1] = 0
+        # divergence
+        if self.past_data[-1] == max(self.past_data) and macd.iloc[-1] < macd.max():
+            signals[2] = -1
+        elif self.past_data[-1] == min(self.past_data) and macd.iloc[-1] > macd.min():
+            signals[2] = 1
+        else:
+            signals[2] = 0
+        return signals
+    
+    def signal_interpretation(self, state: TradingState, signals: MACDsignals) -> int:
+        curr_pos = state.position.get(self.symbol, 0)
+        if signals[2] != 0:
+            return curr_pos + signals[2] * 3
+        if signals[0] == 1 and signals[1] == 1:
+            return curr_pos + 2
+        if signals[0] == 1 and signals[1] == 0:
+            return curr_pos + 1
+        if signals[0] == 1 and signals[1] == -1:
+            return curr_pos # pathological case
+        if signals[0] == 0 and signals[1] == 1:
+            return curr_pos
+        if signals[0] == 0 and signals[1] == 0:
+            return state.position.get(self.symbol, 0)
+        if signals[0] == 0 and signals[1] == -1:
+            return curr_pos
+        if signals[0] == -1 and signals[1] == 1:
+            return curr_pos # pathological case
+        if signals[0] == -1 and signals[1] == 0:
+            return curr_pos - 1
+        if signals[0] == -1 and signals[1] == -1:
+            return curr_pos - 2
+    
+    def act(self, state: TradingState) -> None:
+        order_depth = state.order_depths[self.symbol]
+
+        MACD_signals = self.MACD(state)
+        ideal_pos = self.signal_interpretation(state, MACD_signals)
+
+        pos_diff = max(min(ideal_pos - state.position.get(self.symbol, 0), 50), -50)
+        if pos_diff > 0:
+            for ask, vol in list(order_depth.sell_orders.items()):
+                self.buy(ask,min(-vol, pos_diff))
+
+                pos_diff -= min(-vol, pos_diff)
+                if pos_diff <= 0:
+                    break
+
+        elif pos_diff < 0:
+            for bid, vol in list(order_depth.buy_orders.items()):
+                self.sell(bid,min(vol,-pos_diff))
+                pos_diff += max(-vol, pos_diff)
+                if pos_diff >= 0:
+                    break
+        
+        best_ask = list(order_depth.sell_orders.items())[0][0]
+        best_bid = list(order_depth.buy_orders.items())[0][0]
+        if len(self.past_data) > self.max_sample_size:
+            self.past_data = self.past_data[1:]
+            self.past_data.append((best_ask + best_bid) / 2)
+        else:
+            self.past_data.append((best_ask + best_bid) / 2)
+        
+    
+class SquidStrategy(MACD):
+    def __init__(self, symbol: str, limit: int):
+        super().__init__(symbol,limit,100)
+
+
+# Strategy for Resin: assumes a fixed fair value
 class ResinStrategy(MarketMakingStrategy):
     def get_true_value(self, state: TradingState) -> int:
         return 10_000  # Assumes constant fair value
@@ -255,12 +258,14 @@ class KelpStrategy(MarketMakingStrategy):
         return round((popular_buy_price + popular_sell_price) / 2)  # Mid-price
 
 
+
 # Main trader class that coordinates multiple strategies
 class Trader:
     def __init__(self) -> None:
         limits = {
             "RAINFOREST_RESIN": 50,
             "KELP": 50,
+            "SQUID_INK": 50
         }
 
         # Instantiate strategies for each symbol
@@ -269,9 +274,13 @@ class Trader:
             for symbol, clazz in {
                 "RAINFOREST_RESIN": ResinStrategy,
                 "KELP": KelpStrategy,
+                "SQUID_INK": SquidStrategy
             }.items()
         }
 
+    
+    
+    
     def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
         # logger.print(state.position)  # Log current positions
 
@@ -281,20 +290,20 @@ class Trader:
         old_trader_data = json.loads(state.traderData) if state.traderData != "" else {}
         new_trader_data = {}
 
-        orders = {}
+        result = {}
+
         for symbol, strategy in self.strategies.items():
             if symbol in old_trader_data:
                 strategy.load(old_trader_data.get(symbol, None))
 
             # Run strategy for each symbol if order book is available
             if symbol in state.order_depths:
-                orders[symbol] = strategy.run(state)
+                result[symbol] = strategy.run(state)
+            
+            new_trader_data[symbol] = strategy.save()        
 
-            new_trader_data[symbol] = strategy.save()
 
-        # Serialize internal state for next run
         trader_data = json.dumps(new_trader_data, separators=(",", ":"))
 
-        # Flush logs and return decisions
-        # logger.flush(state, orders, conversions, trader_data)
-        return orders, conversions, trader_data
+
+        return result, conversions, trader_data
